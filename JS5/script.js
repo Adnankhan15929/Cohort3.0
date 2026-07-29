90cd457cacd6cea74fa23ccc049336df7928212d
Adnankhan15929 <khanadnan0339@gmail.com>
2026-07-29 23:53:24 +0530

    settiimeout

--- JS5/script.js
+++ JS5/script.js
@@ -1,2 +1,10 @@
 window.console.log("sync");
-window.alert("nothing")
+// window.alert("nothing")
+window.setTimeout(function (){
+    console.log('2sec');
+},2000)
+
+setTimeout(function (){
+    console.log('1sec');
+},1000)
+console.log("practice");
