804aa6de0be071d9fa50ff219934dbd71edd8242
Adnankhan15929 <khanadnan0339@gmail.com>
2026-08-01 12:56:25 +0530

    fetch

--- JS5/script.js
+++ JS5/script.js
@@ -18,54 +18,68 @@
 
 
 //Promises
-let myOrder = new Promise(function (resolve, reject) {
-    console.log('order is coming...');
- let orderStatus = true
-    setTimeout(function () {
-        if (orderStatus) {
-            console.log('Delivery Done ✅');
-            resolve()
-        } else {
+// let myOrder = new Promise(function (resolve, reject) {
+//     console.log('order is coming...');
+//  let orderStatus = true
+//     setTimeout(function () {
+//         if (orderStatus) {
+//             console.log('Delivery Done ✅');
+//             resolve()
+//         } else {
 
-            reject()
-        }
-    }, 3000)
-})
+//             reject()
+//         }
+//     }, 3000)
+// })
 
-myOrder.then(function () {
-    console.log('Making payment...💸');
+// myOrder.then(function () {
+//     console.log('Making payment...💸');
 
-    let paymentStatus = true 
-    return new Promise(function (res, rej) {
-        setTimeout(function () {
-            if (paymentStatus) {
-                console.log('Payment Done ✅');
-                res()
-            } else {
-                console.log('Payment Cancelled ❌');
-                rej()
-            }
-        }, 2000)
+//     let paymentStatus = true 
+//     return new Promise(function (res, rej) {
+//         setTimeout(function () {
+//             if (paymentStatus) {
+//                 console.log('Payment Done ✅');
+//                 res()
+//             } else {
+//                 console.log('Payment Cancelled ❌');
+//                 rej()
+//             }
+//         }, 2000)
+//     })
+// }).then(function(){
+//     console.log('I am eating food...');
+
+//     let foodStatus = false
+//     return new Promise(function (res, rej) {
+//         setTimeout(function () {
+//             if (foodStatus) {
+//                 console.log('Pet Bhar Gya');
+//                 res()
+//             } else {
+//                 console.log('Pet Nahi bhara');
+//                 rej()
+//             }
+//         }, 3000)
+//     })
+// })
+// .catch(function () {
+//     console.log('Order Cancelled ❌');
+// })
+// .finally(function(){
+//     console.log('Process End');
+// })
+
+
+let response = fetch('https://fakestoreapi.com/products/')
+response
+    .then(function (data) {
+        console.log('Data aa gya ✅');
+        return data.json()
     })
-}).then(function(){
-    console.log('I am eating food...');
-
-    let foodStatus = false
-    return new Promise(function (res, rej) {
-        setTimeout(function () {
-            if (foodStatus) {
-                console.log('Pet Bhar Gya');
-                res()
-            } else {
-                console.log('Pet Nahi bhara');
-                rej()
-            }
-        }, 3000)
+    .then(function (mydata) {
+        console.log(mydata);
     })
-})
-.catch(function () {
-    console.log('Order Cancelled ❌');
-})
-.finally(function(){
-    console.log('Process End');
-})
+    .catch(function () {
+        console.error('Data nhi aaya ❌');
+    })
