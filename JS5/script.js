d01989ebbcc863332be73acd65eeb3d75824d4ea
Adnankhan15929 <khanadnan0339@gmail.com>
2026-08-01 12:52:45 +0530

    promise

--- JS5/script.js
+++ JS5/script.js
@@ -1,10 +1,71 @@
-window.console.log("sync");
-// window.alert("nothing")
-window.setTimeout(function (){
-    console.log('2sec');
-},2000)
+// window.console.log("sync");
+// // window.alert("nothing")
+// window.setTimeout(function (){
+//     console.log('2sec');
+// },2000)
 
-setTimeout(function (){
-    console.log('1sec');
-},1000)
-console.log("practice");
+// setTimeout(function (){
+//     console.log('1sec');
+// },1000)
+// console.log("practice");
+
+
+
+
+
+
+
+
+
+//Promises
+let myOrder = new Promise(function (resolve, reject) {
+    console.log('order is coming...');
+ let orderStatus = true
+    setTimeout(function () {
+        if (orderStatus) {
+            console.log('Delivery Done ✅');
+            resolve()
+        } else {
+
+            reject()
+        }
+    }, 3000)
+})
+
+myOrder.then(function () {
+    console.log('Making payment...💸');
+
+    let paymentStatus = true 
+    return new Promise(function (res, rej) {
+        setTimeout(function () {
+            if (paymentStatus) {
+                console.log('Payment Done ✅');
+                res()
+            } else {
+                console.log('Payment Cancelled ❌');
+                rej()
+            }
+        }, 2000)
+    })
+}).then(function(){
+    console.log('I am eating food...');
+
+    let foodStatus = false
+    return new Promise(function (res, rej) {
+        setTimeout(function () {
+            if (foodStatus) {
+                console.log('Pet Bhar Gya');
+                res()
+            } else {
+                console.log('Pet Nahi bhara');
+                rej()
+            }
+        }, 3000)
+    })
+})
+.catch(function () {
+    console.log('Order Cancelled ❌');
+})
+.finally(function(){
+    console.log('Process End');
+})
